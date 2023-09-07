bd6dbf39d1810cd7e0d83e5038a88396f72e8a28
himanshusainib159 <himanshusainib.159@gmail.com>
2023-09-07 13:54:11 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c94599d1-3104-4cc7-83ad-68e6caded4cc';
+export const MODAL_CLOSE_SEC = 2.5;
